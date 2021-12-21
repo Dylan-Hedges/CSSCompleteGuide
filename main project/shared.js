c98878529acb9d51d621ffa3a1be00f7baac76c6
Dylan-Hedges <d.hedges1@hotmail.co.uk>
2021-12-21 09:07:22 -0500

    Added JS functionality that hides and shows the side menu nav bar used for mobile, added functionality to hide and show the modal (lightbox that appears when CHOOSE PLAN is clicked)

--- main project/shared.js
+++ main project/shared.js
@@ -1,35 +1,55 @@
-//Find elements that have a class of 'backdrop' and saves it to a variable - only keeps the first element it finds
+//Selects the back transparent backdrop - find elements that have a class of 'backdrop' and saves it to a variable - only keeps the first element it finds
 var backdrop = document.querySelector('.backdrop');
-
-//Finds elements that have a class of 'modal' and saves it to a variable - only keeps the first element it finds
+//Selects the modal (lightbox popup)
 var modal = document.querySelector('.modal');
-
-//Find elements that have a class of '.modal__action--negative' and saves it to a variable - only keeps the first element it finds
+//Selects the No button inside the modal
 var modalNoButton = document.querySelector('.modal__action--negative');
-
-//Find elements that have a class of 'plan' and are buttons - saves multiple elements as an array
+//Selects the CHOOSE PLAN buttons - finds elements that have a class of 'plan' and are buttons - saves multiple elements as an array
 var selectPlanButtons = document.querySelectorAll('.plan button');
+//Selects the hamburger icon
+var toggleButton = document.querySelector('.toggle-button');
+//Selects the side nav used for mobiles
+var mobileNav = document.querySelector('.mobile-nav');
 
 //Loops over all select plan buttons
 for (var i = 0; i < selectPlanButtons.length; i++){
   //Adds an event listener to each button that when clicked executes an anonymous function
   selectPlanButtons[i].addEventListener('click', function(){
-    //Makes the modal (lightbox popup) visibile - changes style to block
-    modal.style.display='block';
-    //Makes the black transparent backdrop visible - changes style to block
-    backdrop.style.display='block';
+    //Makes the modal (lightbox popup) visibile - adds the open class to it which changes style to block
+    modal.classList.add('open');
+    //Makes the black transparent backdrop visible - adds the open class to it which changes style to block
+    backdrop.classList.add('open');
+
   });
 }
 
 //Adds an event listener to the black transparent background that when clicked executes the closeModal function
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function(){
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open');
+  closeModal();
+});
 
-//Adds an event listener to the No button that when clicked executes the closeModal function
-modalNoButton.addEventListener('click', closeModal);
+//Checks if No button in the modal exists - prevents errors on Packages and Customers pages that do not have the modal
+if(modalNoButton){
+  //Adds an event listener to the No button that when clicked executes the closeModal function
+  modalNoButton.addEventListener('click', closeModal);
+}
 
 function closeModal(){
-  //Hides the transparent black backdrop
-  backdrop.style.display = "none";
-  //Hides the modal (lightbox)
-  modal.style.display = "none";
+  //If the modal exists - prevents errors on Packages and Customers pages that do not have the modal
+  if(modal){
+    //Hide the modal (lightbox popup) - removes the open class which changes style back to none
+    modal.classList.remove('open');
+  }
+  //Hide the transparent black backdrop - removes the open class which changes style back to none
+  backdrop.classList.remove('open');
 }
+
+//Adds an event listener to the hamburger icon that when clicked executes a function
+toggleButton.addEventListener('click', function(){
+  //Opens the side nav used for mobile devices- adds the open class to it which changes style to block
+  mobileNav.classList.add('open');
+  //Shows the black transparent backdrop - adds the open class to it which changes style to block
+  backdrop.classList.add('open');
+})
